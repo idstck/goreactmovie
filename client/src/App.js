3a786745aadd34c4b0f049eca5f557202cc4f4ec
IDStack <idstack@outlook.co.id>
2022-06-22 22:47:24 +0700

    (client) membuat menu dan content dengan bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,113 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello World!</h1>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='row'>
+				<h1 className='mt-3'>Go react Movie Project!</h1>
+				<hr className='mb-3' />
+			</div>
+			<div className='row'>
+				<div className='col-2'>
+					<div className='card'>
+						<ul className='list-group list-group-flush'>
+							<li className='list-group-item'>An item</li>
+							<li className='list-group-item'>A second item</li>
+							<li className='list-group-item'>A third item</li>
+						</ul>
+					</div>
+				</div>
+				<div className='col-10'>
+					<div className='row'>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-sm-4 mb-2'>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>Special title treatment</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<a href='#' className='btn btn-primary'>
+										Go somewhere
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
