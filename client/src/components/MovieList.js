195274d0371f0f45705e5a70c95997fdbbdcebf2
IDStack <idstack@outlook.co.id>
2022-06-23 12:46:44 +0700

    (client) mengenal state dan hook di react function component

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,92 +1,35 @@
-import React from 'react';
+/* third party */
+import React, { useEffect, useState } from 'react';
 
 const MovieList = () => {
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		setMovies([
+			{ id: 1, title: 'Spongebob Squarepants', runtime: 145 },
+			{ id: 2, title: 'Ogry', runtime: 145 },
+			{ id: 3, title: 'Chalk Zone', runtime: 145 },
+		]);
+	}, []);
+
 	return (
 		<div className='row'>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
+			{movies.map((movie, index) => (
+				<div className='col-sm-4 mb-2' key={index}>
+					<div className='card'>
+						<div className='card-body'>
+							<h5 className='card-title'>{movie.title}</h5>
+							<p className='card-text'>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</p>
+							<a href='#' className='btn btn-primary'>
+								Go somewhere
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className='col-sm-4 mb-2'>
-				<div className='card'>
-					<div className='card-body'>
-						<h5 className='card-title'>Special title treatment</h5>
-						<p className='card-text'>
-							With supporting text below as a natural lead-in to additional
-							content.
-						</p>
-						<a href='#' className='btn btn-primary'>
-							Go somewhere
-						</a>
-					</div>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
