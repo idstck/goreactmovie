4005fc9867742b4132b86302b315c999497e3b12
IDStack <idstack@outlook.co.id>
2022-06-26 14:35:21 +0700

    (server) mendefinisikan entitas model

---
+++ server/models/models.go
@@ -0,0 +1,32 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID          int          `json:"id"`
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	Year        int          `json:"year"`
+	ReleaseDate time.Time    `json:"release_date"`
+	Runtime     int          `json:"runtime"`
+	Rating      int          `json:"rating"`
+	MPAARating  string       `json:"mpaa_rating"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	MovieGenre  []MovieGenre `json:"-"`
+}
+
+type Genre struct {
+	ID        int       `json:"id"`
+	GenreName string    `json:"genre_name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type MovieGenre struct {
+	ID        int       `json:"id"`
+	MovieID   int       `json:"movie_id"`
+	GenreID   int       `json:"genre_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
