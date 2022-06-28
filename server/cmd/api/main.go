ee3491c3894e6b0cfe0c3cdb72c570c8c0c8fb38
IDStack <idstack@outlook.co.id>
2022-06-28 20:14:16 +0700

    (server) create connection to postgres with golang

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
@@ -32,9 +39,16 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on ")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres@localhost/id_gomoviereact?sslmode=disable", "Postgres connection config")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -53,8 +67,25 @@ func main() {
 
 	logger.Printf("Starting sever on port ", cfg.port)
 
-	err := serve.ListenAndServe()
+	err = serve.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
