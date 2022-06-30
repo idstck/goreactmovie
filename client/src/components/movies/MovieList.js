ae17065132585941bf03baebd63acc504864fcd9
IDStack <idstack@outlook.co.id>
2022-06-30 20:52:34 +0700

    (client) menampilkan data movies dari web service

--- client/src/components/movies/MovieList.js
+++ client/src/components/movies/MovieList.js
@@ -1,37 +1,48 @@
 /* third party */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MovieList = () => {
 	const [movies, setMovies] = useState([]);
+	const [loaded, setLoaded] = useState(false);
 
 	useEffect(() => {
-		setMovies([
-			{ id: 1, title: 'Spongebob Squarepants', runtime: 145 },
-			{ id: 2, title: 'Ogry', runtime: 145 },
-			{ id: 3, title: 'Chalk Zone', runtime: 145 },
-		]);
+		const fetchMovies = async () => {
+			const result = await axios(`http://localhost:4000/movies`);
+			await setMovies(result.data.movies);
+			setLoaded(true);
+		};
+		fetchMovies();
 	}, []);
 
 	return (
-		<div className='row'>
-			{movies.map((movie, index) => (
-				<div className='col-sm-4 mb-2' key={index}>
-					<div className='card'>
-						<div className='card-body'>
-							<h5 className='card-title'>{movie.title}</h5>
-							<p className='card-text'>
-								With supporting text below as a natural lead-in to additional
-								content.
-							</p>
-							<Link to={`/movies/${movie.id}`} className='btn btn-primary'>
-								Read more
-							</Link>
-						</div>
-					</div>
+		<>
+			{!loaded ? (
+				<div className='row'>
+					<p>Loading...</p>
 				</div>
-			))}
-		</div>
+			) : (
+				<div className='row'>
+					{movies.map((movie, index) => (
+						<div className='col-sm-4 mb-2' key={index}>
+							<div className='card'>
+								<div className='card-body'>
+									<h5 className='card-title'>{movie.title}</h5>
+									<p className='card-text'>
+										With supporting text below as a natural lead-in to
+										additional content.
+									</p>
+									<Link to={`/movies/${movie.id}`} className='btn btn-primary'>
+										Read more
+									</Link>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
+		</>
 	);
 };
 
