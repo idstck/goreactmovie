c5e8067ac8f04ef80384757e8b86884428904111
IDStack <idstack@outlook.co.id>
2022-07-16 15:28:55 +0700

    (client) menampilkan data genre dari web service ke react

--- client/src/components/GenreList.js
+++ client/src/components/GenreList.js
@@ -1,30 +1,59 @@
 /* third party */
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const GenreList = () => {
 	const [genres, setGenres] = useState([]);
+	const [loaded, setLoaded] = useState(false);
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	const fetchGenres = async () => {
+		try {
+			const result = await axios(`http://localhost:4000/genres`);
+			await setGenres(result.data.genres);
+			setLoaded(true);
+		} catch (err) {
+			setErrorMessage(err.response.data);
+		}
+	};
 
 	useEffect(() => {
-		setGenres([
-			{ id: 1, genre_name: 'Action' },
-			{ id: 2, genre_name: 'Comedy' },
-			{ id: 3, genre_name: 'Crime' },
-		]);
+		fetchGenres();
 	}, []);
 
 	return (
-		<div className='row'>
-			{genres.map((genre, index) => (
-				<div className='col-sm-2 mb-3'>
-					<div className='card' key={index}>
-						<div className='card-body text-center'>
-							<Link to={`/genres/${genre.id}`}>{genre.genre_name}</Link>
+		<>
+			{!loaded ? (
+				(() => {
+					if (errorMessage) {
+						return (
+							<div className='row'>
+								<p>Oops... {errorMessage}</p>
+							</div>
+						);
+					} else {
+						return (
+							<div className='row'>
+								<p>Loading...</p>
+							</div>
+						);
+					}
+				})()
+			) : (
+				<div className='row'>
+					{genres.map((genre, index) => (
+						<div className='col-sm-2 mb-3'>
+							<div className='card' key={index}>
+								<div className='card-body text-center'>
+									<Link to={`/genres/${genre.id}`}>{genre.genre_name}</Link>
+								</div>
+							</div>
 						</div>
-					</div>
+					))}
 				</div>
-			))}
-		</div>
+			)}
+		</>
 	);
 };
 
