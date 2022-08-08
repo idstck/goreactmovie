0ac8dcdfbf3294bbf267552c1999c4b384aa2312
IDStack <idstack@outlook.co.id>
2022-08-08 16:48:37 +0700

    (client) menggunakan react hook form

--- client/src/components/movies/MovieForm.js
+++ client/src/components/movies/MovieForm.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const MovieForm = () => {
+	const { register, handleSubmit, setValue } = useForm();
+	const onSubmit = (data) => console.log(data);
 	return (
 		<>
 			<h2>Movie Form</h2>
 			<hr />
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className='mb-3'>
 					<label htmlFor='' className='form-label'>
 						Title
 					</label>
-					<input type='text' className='form-control' id='title' name='title' />
+					<input
+						type='text'
+						className='form-control'
+						id='title'
+						name='title'
+						{...register('title', { required: true })}
+					/>
 				</div>
 				<div className='mb-3'>
 					<label htmlFor='' className='form-label'>
@@ -21,6 +30,7 @@ const MovieForm = () => {
 						className='form-control'
 						id='release_date'
 						name='release_date'
+						{...register('release_date', { required: true })}
 					/>
 				</div>
 				<div className='mb-3'>
@@ -32,13 +42,19 @@ const MovieForm = () => {
 						className='form-control'
 						id='runtime'
 						name='runtime'
+						{...register('runtime', { required: true })}
 					/>
 				</div>
 				<div className='mb-3'>
 					<label htmlFor='' className='form-label'>
 						MPAA Rating
 					</label>
-					<select name='mpaa_rating' id='mpaa_rating' className='form-control'>
+					<select
+						name='mpaa_rating'
+						id='mpaa_rating'
+						className='form-control'
+						{...register('mpaa_rating', { required: true })}
+					>
 						<option value='G' className='form-select'>
 							G
 						</option>
@@ -65,6 +81,7 @@ const MovieForm = () => {
 						className='form-control'
 						id='rating'
 						name='rating'
+						{...register('rating', { required: true })}
 					/>
 				</div>
 				<div className='mb-3'>
@@ -76,10 +93,13 @@ const MovieForm = () => {
 						className='form-control'
 						id='description'
 						name='description'
+						{...register('description', { required: true })}
 					/>
 				</div>
 				<hr />
-				<button className='btn btn-primary mb-4'>Save</button>
+				<button className='btn btn-primary mb-4' type='submit'>
+					Save
+				</button>
 			</form>
 		</>
 	);
